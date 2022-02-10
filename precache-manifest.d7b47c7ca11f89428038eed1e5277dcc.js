81050a9b7e97060205626da5f9fdf514ac1a087b
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-10 23:20:26 +0200

    Updates

---
+++ precache-manifest.d7b47c7ca11f89428038eed1e5277dcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d617102d32358e670e798766087404",
+    "url": "/accordion-project/index.html"
+  },
+  {
+    "revision": "c65f933dbee9485b1f33",
+    "url": "/accordion-project/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/accordion-project/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordion-project/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65f933dbee9485b1f33",
+    "url": "/accordion-project/static/js/main.bc90e506.chunk.js"
+  },
+  {
+    "revision": "1352c9559679780f921e",
+    "url": "/accordion-project/static/js/runtime-main.7040a17c.js"
+  }
+]);
